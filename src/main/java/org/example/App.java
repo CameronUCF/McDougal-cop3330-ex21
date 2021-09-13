6c629e38a889c92d13c165996f110960d80a4643
Geetis <capiix1@gmail.com>
2021-09-13 15:41:23 -0400

    Added header

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Cameron McDougal
+ */
+
 package org.example;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args )
